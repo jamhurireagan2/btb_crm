68040368bbe426a42cb9e8d2b527cd468f31f3e8
jamhurireagan2 <jamhurireagan2@gmail.com>
2026-07-31 08:30:37 +0300

    edit

--- login.php
+++ login.php
@@ -495,8 +495,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="form-container">
                 <div class="form-header">
                           <div class="brand-logo">
-    <img src="assets/images/cms-logo-red-white.png" alt="Client Management System" style="height: 60px; margin-bottom: 50px;">
-</div>
+<img src="assets/images/cms-logo-red-white.png" alt="Client Management System" width="200"></div>
                     <h3>Sign In</h3>
                     <p>Enter your credentials to access your dashboard</p>
                 </div>
